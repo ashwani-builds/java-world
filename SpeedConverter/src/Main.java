d85c71aa9114b490baac0ebc966e22ac982f600f
Ashwani <ashwani.wuid@gmail.com>
2021-08-18 21:05:42 +0530

    added speedconverter code

---
+++ SpeedConverter/src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        long ans = SpeedConverter.toMilesPerHour(10.5);
+        System.out.println(ans);
+        SpeedConverter.printConversion(10.5);
+    }
+}
