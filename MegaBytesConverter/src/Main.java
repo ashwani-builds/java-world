5522464f225a37370cdf1fa942d4694f2e3d7746
Ashwani <ashwani.wuid@gmail.com>
2021-08-18 21:13:41 +0530

    added megabyte converter code

---
+++ MegaBytesConverter/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        MegaBytesConverter.printMegaBytesAndKiloBytes(25000);
+        MegaBytesConverter.printMegaBytesAndKiloBytes(2028);
+        MegaBytesConverter.printMegaBytesAndKiloBytes(10000);
+
+    }
+}
